75630afb526422f9bba6b91dce6b2c34f0808d6d
ehtisham57 <81214666+ehtisham57@users.noreply.github.com>
2021-10-28 12:02:27 -0700

    half done

---
+++ app.js
@@ -0,0 +1,89 @@
+// // Chapter Alert Question 2
+// alert("Error! Please enter a valid password.");
+
+// // Chapter Alert Question 3
+// alert("Welcome to js land \n Happy Coding!");
+
+// // Chapter Alert Question 4
+// alert("Welcome to Js land....");
+
+// alert("Happy Coding! \n \n Prevent this page from creting additional dialogs.");
+
+// Chapter Alert Question 5
+// alert("Hello.... I can run Js through My Web Browser's Console")
+
+// Chapter Alert Question 7
+// d. Body (after your page’s HTML);
+
+
+
+//-------------------- VARIABLES FOR STRINGS ----------------------------
+
+// Question 1
+
+// var userName;
+// Question 2
+// var myName = "Syed Ehtisham Uddin Balkhi"
+
+// Question 3
+// var message ="Hello World"
+// alert(message);
+
+// Question 4
+// var Name ="Jhone Doe";
+// var age ="15 Years";
+// var certification = "Mobile Web and App Development";
+// alert(Name);
+// alert(age);
+// alert(certification);
+
+
+// Question 5
+// alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// Question 6
+// var email= "ehtishamsyed57@gmail.com"
+// alert("My email address is " + email);
+
+// Question 7
+// var Book= "“A smarter way to learn JavaScript”";
+// alert(Book);
+
+// Question 8
+// var a = "Yah! I Can Write HTML content Through JavaScript";
+// document.write(a);
+
+// Question 9
+
+// var z ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(z);
+
+
+
+
+
+//====================== VARIABLES FOR NUMBERS ===========================
+//Question 1
+// var age =15
+// alert("My age is " + age);
+
+//Question 2
+// var visit ="“You have visited this site N times”.";
+// alert(visit);
+
+//Question 3
+
+// var birthyear = 2000;
+// document.write("My Birth Year is " + birthyear + <br/> + "Data type of my decleard variable is number");
+
+//Question 4
+
+// var a = prompt("Your Name");
+// var b = prompt("Product title");
+// var c = prompt("how many products you wants to order");
+// document.write(" User Name" + a );
+// document.write(" Quantity " + b );
+// document.write(" Orderd " + c );
+
+
+
