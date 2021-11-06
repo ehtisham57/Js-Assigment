0fa905df39d6b30834f8ab1e0d9388a26dcb4f83
ehtisham57 <81214666+ehtisham57@users.noreply.github.com>
2021-11-06 02:27:19 -0700

    second done

--- app.js
+++ app.js
@@ -87,3 +87,158 @@
 
 
 
+//============================VARIABLE NAMES: LEGAL & ILLEGAL===================================
+
+//Queston 1
+
+
+// first = "Ahmed",
+//     second = "Mubeen",
+//     third = "ameen",
+//     var a = first,
+//         second, third;
+// console.log(a);
+
+
+//Question 2
+//legal variable
+
+// var fullnames = 20;
+// var Full-names = 30;
+// var full_names = 50;
+// var fullnames1 = 40;
+// var fullnames$ = 50;
+
+// var a = full_names + Fullnames + fullnames + fullnames1 + fullnames$
+
+// alert(a);
+
+
+//illegal variables
+
+
+
+// var first names = 50;
+// var 1 firstname = 50;
+// var first names = 70;
+// var firstname = 50;
+
+//Question 3
+
+// var a = "“Rules for naming JS variables”";
+// var b = "Variable names can only contain ______, ______,______ and ______.";
+// var c = "Variables must begin with a ______, ______ or_____. For example $name, _name or name";
+// var d = "Variable names are case _________";
+// var e = "Variable names should not be JS _________"
+// document.write(a + "<br/><br/><br/><br/>")
+// document.write(b + "<br/>")
+// document.write(c + "<br/>")
+// document.write(d + "<br/>")
+// document.write(e + "<br/>")
+
+
+
+//==========================MATH EXPRESSIONS=========================
+
+
+//Question 1 
+// var a = 5;
+// var b = 3;
+// var c = a + b
+// document.write("Sum of 5 and 3 is: " + c);
+
+//Question 2
+// var a = 5;
+// var b = 3;
+// var c = a - b
+// document.write("Subtraction of 5 and 3 is: " + c);
+
+// var a = 5;
+// var b = 3;
+// var c = a * b
+// document.write("Multiplication of 5 and 3 is: " + c);
+
+// var a = 5;
+// var b = 3;
+// var c = a / b
+// document.write("Dividion of 5 and 3 is: " + c);
+
+
+//Question 3
+
+// a)
+
+// var a="Value after variable declaration is: ??"
+// document.write(a +"<br>")
+// // b)
+
+// a="Value after deliration is: ??"
+// // c)
+
+// var a=5;
+// document.write("Initial value : "+ a +"<br>")
+// var a = ++a;
+// document.write("value after Increment: "+ a +"<br>")
+// // d), e), f) 
+
+
+// a = a +7
+// document.write("value after addition: "+ a +"<br>")
+// // g), h), i), j), K) ,l)
+
+
+// a = --a;
+// document.write("value after decreament: "+ a +"<br>")
+// a = a % 3
+// document.write("Out Put Is: "+a)
+
+
+//Question 4
+
+// var ticket = 600;
+// var total = ticket*5;
+// document.write("Total Cost to buy a 5 Ticket is : " + total + " PKR")
+
+
+
+//Question 5
+
+
+// var a = 4
+// document.write(a + " X " + " 1 " + " = " + a + "<br>");
+// document.write(a + " X " + " 2 " + " = " + a*2 + "<br>");
+// document.write(a + " X " + " 3 " + " = " + a*3 + "<br>");
+// document.write(a + " X " + " 4 " + " = " + a*4 + "<br>");
+// document.write(a + " X " + " 5 " + " = " + a*5 + "<br>");
+// document.write(a + " X " + " 6 " + " = " + a*6 +  "<br>");
+// document.write(a + " X " + " 7 " + " = " + a*7 + "<br>");
+// document.write(a + " X " + " 8 " + " = " + a*8 + "<br>");
+// document.write(a + " X " + " 9 " + " = " + a*9 + "<br>");
+// document.write(a + " X " + " 10 " + " = " + a*10 + "<br>");
+
+
+//Question 6
+
+// var c = 30
+// f = 50
+// var f =(c *9/5) + 32 
+// var c =(f - 30)*5/9
+// document.write("30 degree of celcius is : " + f + "<br>")
+// document.write("50 degree of ferhenhite is : " + c)
+
+// Question 7
+
+
+// var item1 = 650;
+// document.write("Price Of Item 1 is: "+ item1 +"<br>");
+// var Quan1 =3;
+// document.write("Quantities Of Item 1 is : "+ Quan1 +"<br>");
+// var item2 = 100;
+// document.write("Price Of Item 1 is: "+ item2 +"<br>");
+// var Quan2 =7;
+// document.write("Quantities Of Item 1 is : "+ Quan2 +"<br>");
+// var charges = 100
+// document.write("Shiping Charges is " + charges + "<br><br><br><br>")
+
+// var total= item1*Quan1 + item2*Quan2 + charges
+// document.write("Total Cost of Yor Orader Is "+total)
